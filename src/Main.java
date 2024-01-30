cfd88e6f2760c798d4b7dc8e856468a2036ca51d
Fernando <fernandophdez@gmail.com>
2024-01-30 17:37:17 -0600

    Creacion del proyecto

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public class operadores {
+        int operador1 =0,operador2 = 0;
+        
+
+        public int suma (int op1, int op2){
+            int resultado=0;
+
+            resultado = op1 + op2;
+
+            return resultado;
+        }
+        public int resta (int op1, int op2){
+            int resultado=0;
+
+            resultado = op1 - op2;
+
+            return resultado;
+        }
+        public int multiplicacion (int op1, int op2){
+            int resultado=0;
+
+            resultado = op1 * op2;
+
+            return resultado;
+        }
+        public int division (int op1, int op2){
+            int resultado=0;
+
+            resultado = op1 / op2;
+
+            return resultado;
+        }
+        public int modulo (int op1, int op2){
+            int resultado=0;
+
+            resultado = op1 % op2;
+
+            return resultado;
+        }
+;    }
+}
