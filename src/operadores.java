0618c61d211a5444d48502cf1121e4085cb596be
Fernando <fernandophdez@gmail.com>
2024-01-30 17:49:32 -0600

    agregue una impresion en pantalla en cada metodo de la clase operadores

--- src/operadores.java
+++ src/operadores.java
@@ -1,38 +1,38 @@
 public class operadores {
-    
+
     public int suma (int op1, int op2){
         int resultado=0;
 
         resultado = op1 + op2;
-
+        System.out.println("El resultado de la suma es:" + resultado);
         return resultado;
     }
     public int resta (int op1, int op2){
         int resultado=0;
 
         resultado = op1 - op2;
-
+        System.out.println("El resultado de la resta es:" + resultado);
         return resultado;
     }
     public int multiplicacion (int op1, int op2){
         int resultado=0;
 
         resultado = op1 * op2;
-
+        System.out.println("El resultado de la multiplicacion es:" + resultado);
         return resultado;
     }
     public int division (int op1, int op2){
         int resultado=0;
 
         resultado = op1 / op2;
-
+        System.out.println("El resultado de la division es:" + resultado);
         return resultado;
     }
     public int modulo (int op1, int op2){
         int resultado=0;
 
         resultado = op1 % op2;
-
+        System.out.println("El resultado de la modulo es:" + resultado);
         return resultado;
     }
 }
