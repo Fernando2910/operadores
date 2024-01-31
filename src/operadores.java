8927d9d10d65e0d70594378378a5e69704c743ed
Fernando <fernandophdez@gmail.com>
2024-01-30 18:05:01 -0600

    Modifique el metodo de division para que solo imprima 2 decimales

--- src/operadores.java
+++ src/operadores.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class operadores {
 
     public int suma (int op1, int op2){
@@ -22,10 +24,11 @@ public class operadores {
         return resultado;
     }
     public float division (float op1, float op2){
+        DecimalFormat formato1 = new DecimalFormat("#.00");
         float resultado=0;
 
         resultado = op1 / op2;
-        System.out.println("El resultado de la division es:" + resultado);
+        System.out.println("El resultado de la division es:" + formato1.format(resultado));
         return resultado;
     }
     public int modulo (int op1, int op2){
